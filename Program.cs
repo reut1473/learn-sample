1f2dced025bfefe9f01e46f2abee3de7c9b2df23
reut1473 <reutp1999@gmail.com>
2019-07-18 13:23:01 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int[][] solution;
+            solution=getsolution();
+            foreach (var item in solution)
+            {
+                foreach (var item1 in item)
+                {
+                    Console.Write(item1 == 0 ? '-':'Q');
+                }
+                Console.WriteLine();
+            }
+        
+        }
+         static int[][] getsolution()
+        {
+           
+            return;
+        }
+    }
+}
